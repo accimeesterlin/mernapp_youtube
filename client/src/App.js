ebb11071baf9239e62d69bdc62ad9d2b7699cb51
Esterling Accime <esterling.accime@coxautoinc.com>
2019-11-13 03:03:05 -0500

    change app title

--- client/src/App.js
+++ client/src/App.js
@@ -86,7 +86,7 @@ class App extends React.Component {
     //JSX
     return(
       <div className="app">
-        <h2>Welcome to my App</h2>
+        <h2>Welcome to the best App ever</h2>
         <form onSubmit={this.submit}>
           <div className="form-input">
             <input 
